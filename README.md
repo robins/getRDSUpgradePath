b5369b14aa20ba942fde4b5c3002445e9a4902da
Robins <tharakan@gmail.com>
2018-05-24 00:02:22 +1000

    Show sample runs in README. Get Script to work (at least suboptimally)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # getRDSUpgradePath
 Python script to find all possible Upgrade paths (from one Version to another) for AWS RDS Database Engines
+
+Sample Runs
+```
+C:\proj\aws_rds_postgres_upgrade_path>python getUpgradePath.py 9.3.12 9.6.6
+From: 9.5.10 To:9.6.6
+From: 9.4.15 To:9.5.10
+From: 9.3.12 To:9.4.15
+
+C:\proj\aws_rds_postgres_upgrade_path>python getUpgradePath.py 9.3.12
+Syntax: python getUpgradePath.py v1 v2 [engine]
+Source / Destination Versions are Mandatory. You may also optionally mention Engine (default Postgres)
+```
--- getUpgradePath.py
+++ getUpgradePath.py
@@ -1,21 +1,15 @@
 import sys
 import boto3
 
-lookup.clear()
-
 def callaws(arg, dest, engine):
 
-  if (get(d[arg]) == None):
-    resp = client.describe_db_engine_versions(
-      Engine=engine,
-      EngineVersion=arg,
-    )
+  resp = client.describe_db_engine_versions(
+    Engine=engine,
+    EngineVersion=arg,
+  )
 
-    if (len(resp['DBEngineVersions'][0]['ValidUpgradeTarget']) == 0):
-      return 0
-
-    for k in resp['DBEngineVersions'][0]['ValidUpgradeTarget']:
-      
+  if (len(resp['DBEngineVersions'][0]['ValidUpgradeTarget']) == 0):
+    return 0
 
   for k in reversed(resp['DBEngineVersions'][0]['ValidUpgradeTarget']):
     k2 = k['EngineVersion']
@@ -23,14 +17,15 @@ def callaws(arg, dest, engine):
       print ('From: ' + arg + ' To:' + k2)
       return 1
       
-if not (len(sys.argv) >= 3 and len(sys.argv) <= 4):
+if len(sys.argv) == 3:
+  engine = 'postgres'
+elif len(sys.argv) == 4:
+  engine = sys.argv[3]
+else:
   print('Syntax: python getUpgradePath.py v1 v2 [engine]')
   print('Source / Destination Versions are Mandatory. You may also optionally mention Engine (default Postgres)')
   sys.exit()
 
 client= boto3.client('rds')
-if len(sys.argv) == 3:
-  engine = 'postgres'
-
 if (callaws(sys.argv[1], sys.argv[2], engine) == 0):
   print("Unable to find Upgrade path")
