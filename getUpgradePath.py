34b72e2acbb56fa8aa1e95960eacab55e52dd89a
Robins <tharakan@gmail.com>
2018-05-27 16:25:33 +1000

    Add some comments to explain why the script is doing, what it's doing

--- getUpgradePath.py
+++ getUpgradePath.py
@@ -3,14 +3,19 @@ import boto3
 
 def callaws(arg, dest, engine):
 
+  client = boto3.client('rds')
+
+  # Ideally this takes non-postgres engines as well, although not well tested (XXX)
   resp = client.describe_db_engine_versions(
     Engine=engine,
     EngineVersion=arg,
   )
 
+  # If there are no Upgrade paths, return right away
   if (len(resp['DBEngineVersions'][0]['ValidUpgradeTarget']) == 0):
     return 0
 
+  # Until caching comes in, process the list in reversed order (optimal)
   for k in reversed(resp['DBEngineVersions'][0]['ValidUpgradeTarget']):
     k2 = k['EngineVersion']
     if ((k2 == dest) or (callaws(k2, dest, engine) == 1)):
@@ -18,6 +23,7 @@ def callaws(arg, dest, engine):
       return 1
   return 0
 
+# Currently the default engine is postgres, unless explicitly provided (as 4th Option)
 if len(sys.argv) == 3:
   engine = 'postgres'
 elif len(sys.argv) == 4:
@@ -27,6 +33,5 @@ else:
   print('Source / Destination Versions are Mandatory. You may also optionally mention Engine (default Postgres)')
   sys.exit()
 
-client= boto3.client('rds')
 if (callaws(sys.argv[1], sys.argv[2], engine) == 0):
   print("Unable to find Upgrade path")
