91376308c42925c414c2a96cf6c9a4b56febf07b
Robins <tharakan@gmail.com>
2019-09-25 13:21:26 +1000

    WIP: Using a dictionary to cache query results to find long combination paths

--- getUpgradePath.py
+++ getUpgradePath.py
@@ -4,7 +4,21 @@
 import sys
 import boto3
 
+lookup = {}
+
 def callaws(arg, dest, engine, just_check):
+
+  print ('============')
+  print ('Checking combination ' + arg + '-' + dest)
+
+  v = arg + '-' + dest
+  if (v in lookup):
+    if (lookup[v] == 1):
+      print ('Cached: Combination possible')
+      return 1
+    else:
+      print ('Cached: Combination not possible')
+      return 0
 
   client = boto3.client('rds')
 
@@ -21,12 +35,25 @@ def callaws(arg, dest, engine, just_check):
   if (just_check):
     return 1
 
-  # Until caching comes in, process the list in reversed order (optimal)
+  k2 = []
   for k in reversed(resp['DBEngineVersions'][0]['ValidUpgradeTarget']):
-    k2 = k['EngineVersion']
-    if ((k2 == dest) or (callaws(k2, dest, engine, 0) == 1)):
-      print ('Upgrade From: ' + arg + ' To: ' + k2)
+    k2.append(k['EngineVersion'])
+    v = arg + '-' + k['EngineVersion']
+    if (not v in lookup):
+      lookup[v] = 1
+
+  print ('Valid targets: ' + '  '.join(k2))
+  print ('Lookup: ' + '  '.join(lookup))
+
+  # Process the list in reversed order since ideally 
+  # the target is expected to be a recent Minor Version
+  for k in (k2):
+    if ((k == dest) or (callaws(k, dest, engine, 0) == 1)):
+      print ('Upgrade From: ' + arg + ' To: ' + k)
       return 1
+
+  v = arg + '-' + dest
+  lookup[v] = -1
   return 0
 
 # Currently the default engine is postgres, unless explicitly provided (as 4th Option)
@@ -39,14 +66,13 @@ else:
   print('Source / Destination Versions are Mandatory. You may also optionally mention Engine (default Postgres)')
   sys.exit()
 
-if (callaws(sys.argv[1], '9.1', engine, 1) == 0):
+if (callaws(sys.argv[1], 'x', engine, 1) == 0):
   print("Unable to find Upgrade path. Is the Source version supported in RDS?")
   sys.exit() 
 
-if (callaws(sys.argv[2], '9.1', engine, 1) == 0):
+if (callaws(sys.argv[2], 'y', engine, 1) == 0):
   print("Unable to find Upgrade path. Is the Target version supported in RDS?")
   sys.exit()
 
-if ((callaws(sys.argv[2], '9.1', engine, 0) == 0) 
-  or (callaws(sys.argv[1], sys.argv[2], engine) == 0)):
+if (callaws(sys.argv[1], sys.argv[2], engine, 0) == 0):
   print("Unable to find Upgrade path")
