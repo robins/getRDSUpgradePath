3e6812a8fa3c88a4fe4f5e408f01cce39ac8ac0a
Robins Tharakan <tharakan@gmail.com>
2019-11-25 23:08:35 +0000

    Use python in script to generate sample output

--- gen_readme_output.sh
+++ gen_readme_output.sh
@@ -1,9 +1,9 @@
-py getUpgradePath.py 9.3.12 10.4 postgres
-py getUpgradePath.py 5.5.46 5.7.21 mysql
-py getUpgradePath.py 11.2.0.4.v1 12.1.0.2.v11 oracle-ee
-py getUpgradePath.py 10.50.6000.34.v1 14.00.3015.40.v1 sqlserver-ee
-py getUpgradePath.py 10.0.17 10.2.12 mariadb
-py getUpgradePath.py 9.6.3 9.6.6 aurora-postgresql
-py getUpgradePath.py 9.3.12 10.3 postgres
-py getUpgradePath.py 9.3.12
-py getUpgradePath.py 9.3.12 10.9 postgres 1
+python getUpgradePath.py 9.3.12 10.4 postgres
+python getUpgradePath.py 5.5.46 5.7.21 mysql
+python getUpgradePath.py 11.2.0.4.v1 12.1.0.2.v11 oracle-ee
+python getUpgradePath.py 10.50.6000.34.v1 14.00.3015.40.v1 sqlserver-ee
+python getUpgradePath.py 10.0.17 10.2.12 mariadb
+python getUpgradePath.py 9.6.3 9.6.6 aurora-postgresql
+python getUpgradePath.py 9.3.12 10.3 postgres
+python getUpgradePath.py 9.3.12
+python getUpgradePath.py 9.3.12 10.9 postgres 1
