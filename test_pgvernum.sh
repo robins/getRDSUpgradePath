c83af8542e397eddcdf8d75ab10f77cd43bb33eb
Robins Tharakan <tharakan@gmail.com>
2019-11-29 12:47:08 +1100

    Use python instead of py

--- test_pgvernum.sh
+++ test_pgvernum.sh
@@ -1,12 +1,11 @@
 
-
-py pgvernum.py 9.3.14
+python pgvernum.py 9.3.14
 # 90314
-py pgvernum.py 9.6.1
+#py pgvernum.py 9.6.1
 # 90601
-py pgvernum.py 10.0
+#py pgvernum.py 10.0
 # 100000
-py pgvernum.py 10.14
+#py pgvernum.py 10.14
 # 100014
-py pgvernum.py 11.1
+#py pgvernum.py 11.1
 # 110001
