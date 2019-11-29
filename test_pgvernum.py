60bc9579ea665aeac25e8773162d80aa096ef3b0
Robins Tharakan <tharakan@gmail.com>
2019-11-29 13:04:50 +1100

    Add coverage dependency. Test passing locally. Lets try travis

---
+++ test_pgvernum.py
@@ -0,0 +1,21 @@
+import unittest
+import pgvernum
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(pgvernum.getPGVersionString('9.3.14'), 90314)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+#python pgvernum.py 9.3.14
+# 90314
+#py pgvernum.py 9.6.1
+# 90601
+#py pgvernum.py 10.0
+# 100000
+#py pgvernum.py 10.14
+# 100014
+#py pgvernum.py 11.1
+# 110001
