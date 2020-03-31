2ed97b1fa4b8b105bb9ba1b57435ea99a377234b
Robins Tharakan <tharakan@gmail.com>
2020-03-31 17:08:25 +1100

    Correct a test case. All green

--- test_getUpgradePath.py
+++ test_getUpgradePath.py
@@ -5,7 +5,7 @@ from getUpgradePath import main
 
 class TestMethods(unittest.TestCase):
   def test_main(self):
-    self.assertEqual(main(['getUpgradePath.py', '9.4.23','9.5.15', 'postgres', '1', '0']), "Upgrade path not found")
+    self.assertEqual(main(['getUpgradePath.py', '9.4.23','9.5.15', 'postgres', '1', '0']), "\nUpgrade path not found. May be you want to increase hop-count and try again.")
     self.assertEqual(main(['getUpgradePath.py', '9.3.14','9.3.16', 'postgres', '1', '0']), "['9.3.14', '9.3.16']")
     self.assertEqual(main(['getUpgradePath.py', '9.4.23','9.5.18', 'postgres', '1', '0']), "['9.4.23', '9.5.18']")
 
